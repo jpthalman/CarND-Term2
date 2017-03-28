eca49c6a066aa027340e43447bc8622059a9f5d6
jpthalman <jpthalman@gmail.com>
2017-03-27 22:43:10 -0600

    Initial commit

--- ExtendedKalmanFilter/src/sensor_data_packet.h
+++ ExtendedKalmanFilter/src/sensor_data_packet.h
@@ -1,17 +1,22 @@
+//
+// Created by japata on 3/27/17.
+//
+
 #ifndef EXTENDEDKALMANFILTER_SENSOR_DATA_PACKET_H
 #define EXTENDEDKALMANFILTER_SENSOR_DATA_PACKET_H
 
 #include "Eigen/Dense"
 
+
 struct SensorDataPacket {
-    long long timestamp_;
+    long long timestamp;
 
     enum SensorType {
         RADAR,
         LIDAR
-    } sensor_type_;
+    } sensor_type;
 
-    Eigen::VectorXd values_;
+    Eigen::VectorXd values;
 };
 
 #endif //EXTENDEDKALMANFILTER_SENSOR_DATA_PACKET_H
