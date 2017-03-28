74f86e4828199c22ef208c9b2d6b97127b960020
jpthalman <jpthalman@gmail.com>
2017-03-27 23:15:34 -0600

    Add basic kalman filter class

---
+++ ExtendedKalmanFilter/src/kalman_filter.cpp
@@ -0,0 +1,24 @@
+//
+// Created by japata on 3/27/17.
+//
+
+#include "kalman_filter.h"
+
+
+using namespace Eigen;
+
+KalmanFilter::KalmanFilter() {}
+
+
+KalmanFilter::~KalmanFilter() {}
+
+
+void KalmanFilter::Predict() {
+    x_ = H_ * x_;
+    P_ = H_ * P_ * H_.transpose() + Q_;
+}
+
+
+void KalmanFilter::Update(const Eigen::VectorXd &z, const SensorDataPacket::SensorType sensor_type) {
+    // TODO
+}
---
+++ ExtendedKalmanFilter/src/kalman_filter.h
@@ -0,0 +1,55 @@
+//
+// Created by japata on 3/27/17.
+//
+
+#ifndef EXTENDEDKALMANFILTER_KALMAN_FILTER_H
+#define EXTENDEDKALMANFILTER_KALMAN_FILTER_H
+
+#include "Eigen/Dense"
+#include "sensor_data_packet.h"
+
+
+class KalmanFilter {
+public:
+    // state vector
+    Eigen::VectorXd x_;
+
+    // state covariance matrix
+    Eigen::MatrixXd P_;
+
+    // state transition matrix
+    Eigen::MatrixXd F_;
+
+    // environment covariance matrix
+    Eigen::MatrixXd Q_;
+
+    // sensor matrix
+    Eigen::MatrixXd H_;
+
+    // sensor covariance matrix
+    Eigen::MatrixXd R_;
+
+    /**
+     * Constructor
+     * */
+    KalmanFilter();
+
+    /**
+     * Destructor
+     * */
+    virtual ~KalmanFilter();
+
+    /**
+     * Predict state k+1 based upon the current state vector (x_), state covariance (P_), and environment noise (Q_).
+     * */
+    void Predict();
+
+    /***/
+    void Update(const Eigen::VectorXd &z, const SensorDataPacket::SensorType sensor_type);
+
+private:
+    const Eigen::MatrixXd I_ = Eigen::MatrixXd::Identity(2, 2);
+};
+
+
+#endif //EXTENDEDKALMANFILTER_KALMAN_FILTER_H
