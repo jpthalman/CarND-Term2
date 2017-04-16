67fc7d2621a07e2b183bb36ae86402d3f0b1df29
Jacob <jpthalman@gmail.com>
2017-04-15 19:39:27 -0600

    Start to flesh out the class

--- UnscentedKalmanFilter/src/unscented_kalman_filter.cpp
+++ UnscentedKalmanFilter/src/unscented_kalman_filter.cpp
@@ -3,19 +3,64 @@
 //
 
 #include "unscented_kalman_filter.h"
+#include "tools.h"
 
 using namespace Eigen;
 
-
-UnscentedKalmanFilter::UnscentedKalmanFilter() {
-    // TODO
-}
 
 UnscentedKalmanFilter::~UnscentedKalmanFilter() {
     // TODO
 }
 
-VectorXd UnscentedKalmanFilter::ProcessMeasurement(SensorDataPacket &data) {
+void UnscentedKalmanFilter::ProcessMeasurement(SensorDataPacket &data) {
+    /**********************************************************************************
+     *                                    Initialize
+     **********************************************************************************/
+    if (!is_initialized_) {
+        if (data.sensor_type == SensorDataPacket::LIDAR) {
+            x_.fill(0.0);
+            x_(0) = data.observations(0);  // px
+            x_(1) = data.observations(1);  // py
+            x_(3) = atan2(x_(1), x_(0));   // yaw
+        }
+        else if (data.sensor_type == SensorDataPacket::RADAR) {
+            VectorXd cartesian = polar_to_cartesian(data.observations);
+
+            x_.fill(0.0);
+            x_(0) = cartesian(0);  // px
+            x_(1) = cartesian(1);  // py
+            x_(2) = sqrt(pow(x_(0), 2) + pow(x_(1), 2));  // v
+            x_(3) = atan2(x_(1), x_(0));
+            // TODO: estimate yaw rate with velocities
+        }
+
+        prev_timestamp_ = data.timestamp;
+        is_initialized_ = true;
+    }
+
+    /**********************************************************************************
+     *                                     Predict
+     **********************************************************************************/
+
+    // create augmented mean vector including the means of the noise processes
+    VectorXd x_aug;
+    x_aug << x_, 0, 0;
+
     // TODO
-    return x_;
+
+    /**********************************************************************************
+     *                                      Update
+     **********************************************************************************/
+
+    // TODO
+
+    // store the UKF prediction in the referenced data packet
+    data.predictions = x_;
+}
+
+void UnscentedKalmanFilter::GenerateSigmaPoints(Eigen::VectorXd x, Eigen::MatrixXd P, int lambda) {
+    double scaling_factor = sqrt(n_states_ + lambda);
+    MatrixXd P_sqrt = P.llt().matrixL();
+
+    sigma_points_ << x, (scaling_factor * P_sqrt).colwise() + x, (-scaling_factor * P_sqrt).colwise() + x;
 }
--- UnscentedKalmanFilter/src/unscented_kalman_filter.h
+++ UnscentedKalmanFilter/src/unscented_kalman_filter.h
@@ -5,26 +5,52 @@
 #ifndef UNSCENTEDKALMANFILTER_UNSCENTED_KALMAN_FILTER_H
 #define UNSCENTEDKALMANFILTER_UNSCENTED_KALMAN_FILTER_H
 
+#include <vector>
 #include "sensor_data_packet.h"
 #include "Eigen/Dense"
 
 
 class UnscentedKalmanFilter {
 public:
-    UnscentedKalmanFilter();
+    UnscentedKalmanFilter(int init_n_states, std::vector<double> noise_vector) :
+            n_states_(init_n_states),
+            x_(Eigen::VectorXd(init_n_states)),
+            P_(Eigen::MatrixXd::Identity(init_n_states, init_n_states)),
+            sigma_points_(Eigen::MatrixXd(init_n_states, 2 * init_n_states + 1))
+    {
+        // Initialize noise covariances of the state covariance.
+        // For example, if we have 3 states and one of them is noise, then P_ will look like:
+        //
+        // 1  0  0
+        // 0  1  0
+        // 0  0  noise_vector[0]^2
+        for (int idx = init_n_states - noise_vector.size(); idx < init_n_states; ++idx) {
+            P_(idx, idx) = pow(noise_vector[idx], 2);
+        }
+    }
+
     virtual ~UnscentedKalmanFilter();
 
-    Eigen::VectorXd GetCurrentState() const { return x_; }
-    Eigen::MatrixXd GetCurrentCovariance() const { return P_; }
+    Eigen::VectorXd GetCurrentState() const { return x_.head(5); }
+    Eigen::MatrixXd GetCurrentCovariance() const { return P_.block<5,5>(0, 0); }
 
-    Eigen::VectorXd ProcessMeasurement(SensorDataPacket &data);
+    void ProcessMeasurement(SensorDataPacket &data);
 
 private:
+    void GenerateSigmaPoints(Eigen::VectorXd x, Eigen::MatrixXd P, int lambda);
+
+    int n_states_;
+    bool is_initialized_ = false;
+    long long prev_timestamp_;
+
     // state vector
     Eigen::VectorXd x_;
 
     // state covariance
     Eigen::MatrixXd P_;
+
+    // sigma points
+    Eigen::MatrixXd sigma_points_;
 };
 
 
