56f2e4dccb12d9a10cd778a96c883689203dcda0
jpthalman <jpthalman@gmail.com>
2017-04-19 15:04:54 -0600

    Ad RMSE and argument checking functions

--- UnscentedKalmanFilter/src/tools.cpp
+++ UnscentedKalmanFilter/src/tools.cpp
@@ -3,8 +3,12 @@
 //
 
 #include "tools.h"
+#include <fstream>
 
-Eigen::VectorXd polar_to_cartesian(Eigen::VectorXd &polar) {
+using namespace std;
+using namespace Eigen;
+
+Eigen::VectorXd polar_to_cartesian(const Eigen::VectorXd &polar) {
     double rho = polar(0);
     double phi = polar(1);
     double rho_dot = polar(2);
@@ -15,4 +19,66 @@ Eigen::VectorXd polar_to_cartesian(Eigen::VectorXd &polar) {
             rho_dot * cos(phi),     // vx
             rho_dot * sin(phi);     // vy
     return cartesian;
+}
+
+Eigen::VectorXd calculate_rmse(std::vector<SensorDataPacket> &data_packets)
+{
+    size_t n_obs = data_packets.size();
+    VectorXd rmse = Vector4d::Zero();
+
+    if (n_obs == 0)
+    {
+        cerr << "calculate_rmse | There are no data packets." << endl;
+        return rmse;
+    }
+
+    Vector4d resid;
+    for (size_t i = 0; i < n_obs; ++i)
+    {
+        resid  = data_packets[i].ground_truths - data_packets[i].predictions;
+        resid = resid.array() * resid.array();
+        rmse += resid;
+    }
+
+    rmse /= n_obs;
+
+    return rmse.array().sqrt();
+}
+
+void check_arguments(int argc, char* argv[])
+{
+    string usage_instructions = "Usage instructions: ";
+    usage_instructions += argv[0];
+    usage_instructions += " path/to/infile.txt path/to/outfile.txt";
+
+    bool has_valid_flags = false;
+
+    if (argc == 1)
+        cerr << usage_instructions << endl;
+    else if (argc == 2)
+        cerr << "Please include and output file.\n" << usage_instructions << endl;
+    else if (argc == 3)
+        has_valid_flags = true;
+    else
+        cerr << "Too many arguments.\n" << usage_instructions << endl;
+
+    if (!has_valid_flags)
+        exit(EXIT_FAILURE);
+}
+
+void check_files(
+        const ifstream &infile,
+        const string &in_name,
+        const ofstream &outfile,
+        const string &out_name)
+{
+    if (!infile.is_open()) {
+        cerr << "Cannot open input file: " << in_name << endl;
+        exit(EXIT_FAILURE);
+    }
+
+    if (!outfile.is_open()) {
+        cerr << "Cannot open output file: " << out_name << endl;
+        exit(EXIT_FAILURE);
+    }
 }
--- UnscentedKalmanFilter/src/tools.h
+++ UnscentedKalmanFilter/src/tools.h
@@ -5,8 +5,21 @@
 #ifndef UNSCENTEDKALMANFILTER_TOOLS_H
 #define UNSCENTEDKALMANFILTER_TOOLS_H
 
+#include <iostream>
+#include <vector>
 #include "Eigen/Dense"
+#include "sensor_data_packet.h"
 
-Eigen::VectorXd polar_to_cartesian(Eigen::VectorXd &polar);
+Eigen::VectorXd polar_to_cartesian(const Eigen::VectorXd &polar);
+
+Eigen::VectorXd calculate_rmse(std::vector<SensorDataPacket> &data_packets);
+
+void check_arguments(int argc, char* argv[]);
+
+void check_files(
+        const std::ifstream &infile,
+        const std::string &in_name,
+        const std::ofstream &outfile,
+        const std::string &out_name);
 
 #endif //UNSCENTEDKALMANFILTER_TOOLS_H
