35c42319764e86c2325626c9369272e7e4fe66fc
jpthalman <jpthalman@gmail.com>
2017-04-23 16:54:19 -0600

    Change outfile formatting to CSV and output the NIS

--- UnscentedKalmanFilter/src/main.cpp
+++ UnscentedKalmanFilter/src/main.cpp
@@ -11,8 +11,8 @@ int main(int argc, char* argv[])
     check_arguments(argc, argv);
 
     string in_name = argv[1], out_name = argv[2];
-    ifstream infile(in_name.c_str(), ifstream::in);
-    ofstream outfile(out_name.c_str(), ofstream::out);
+    ifstream infile(in_name.c_str());
+    ofstream outfile(out_name.c_str());
 
     check_files(infile, in_name, outfile, out_name);
 
@@ -36,12 +36,14 @@ int main(int argc, char* argv[])
             0.15   // py
     };
     vector<float> process_noise{
-            0.2,  // acceleration
-            0.2   // yaw rate
+            3.0,  // acceleration
+            2.0   // yaw rate
     };
-    double lambda = -5;
+    double lambda = -4;
 
     RadarLidarUKF ukf(radar_noise, lidar_noise, process_noise, lambda);
+
+    outfile << "SensorType,Px_pred,Py_pred,Vx_pred,Vy_pred,Px_meas,Py_meas,Px_gt,Py_gt,Vx_gt,Vy_gt,NIS" << endl;
 
     for (auto &data_packet : data_packets)
     {
@@ -49,29 +51,28 @@ int main(int argc, char* argv[])
 
         ukf.ProcessMeasurement(data_packet);
 
-        line_out << (data_packet.sensor_type == SensorDataPacket::RADAR ? "R" : "L") << "\t";
-        line_out << "--\t";
+        line_out << (data_packet.sensor_type == SensorDataPacket::RADAR ? "R" : "L") << ",";
 
         for (size_t i = 0; i < data_packet.predictions.size(); ++i)
-            line_out << data_packet.predictions(i) << "\t";
-        line_out << "--\t";
+            line_out << data_packet.predictions(i) << ",";
 
         if (data_packet.sensor_type == SensorDataPacket::LIDAR)
         {
-            line_out << data_packet.observations(0) << "\t"  // px
-                     << data_packet.observations(1) << "\t"; // py
+            line_out << data_packet.observations(0) << ","  // px
+                     << data_packet.observations(1) << ","; // py
         }
         else if (data_packet.sensor_type == SensorDataPacket::RADAR)
         {
             Eigen::VectorXd cartesian = polar_to_cartesian(data_packet.observations);
 
-            line_out << cartesian(0) << "\t"  // px
-                     << cartesian(1) << "\t"; // py
+            line_out << cartesian(0) << ","  // px
+                     << cartesian(1) << ","; // py
         }
-        line_out << "--\t";
 
         for (size_t i = 0; i < data_packet.ground_truths.size(); ++i)
-            line_out << data_packet.ground_truths(i) << "\t";
+            line_out << data_packet.ground_truths(i) << ",";
+
+        line_out << data_packet.net_innovation_score;
         outfile << line_out.str() << endl;
     }
 
