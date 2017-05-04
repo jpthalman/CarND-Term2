9317534cc8581f8662025f3b2ea350ff101cbf61
jpthalman <jpthalman@gmail.com>
2017-05-03 23:14:45 -0600

    Tune PID values

--- PID-Controller/src/main.cpp
+++ PID-Controller/src/main.cpp
@@ -33,7 +33,7 @@ int main()
   uWS::Hub h;
 
   // TODO: tune these values
-  PID pid(0.0, 0.1, 0.1, 0.001);
+  PID pid(0.0, 0.1, 0.0005, 0.7);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -67,7 +67,7 @@ int main()
 
           json msgJson;
           msgJson["steering_angle"] = steer_value;
-          msgJson["throttle"] = 0.3;
+          msgJson["throttle"] = 0.4;
           auto msg = "42[\"steer\"," + msgJson.dump() + "]";
           std::cout << msg << std::endl;
           ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
