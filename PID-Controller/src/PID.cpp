7942b340d78923c26b5df7744b135b6da56c22aa
jpthalman <jpthalman@gmail.com>
2017-05-09 23:07:37 -0600

    Add function to reset terms

--- PID-Controller/src/PID.cpp
+++ PID-Controller/src/PID.cpp
@@ -9,36 +9,34 @@ PID::PID(double target, double kp_init, double ki_init, double kd_init) :
     // initialize errors to zero
     p_error_ = i_error_ = d_error_ = 0;
 
-    // initialize timestamp to zero
-    prev_timestamp_ = 0;
-
     // set max integral to a really big value
     max_integral_ = 1e9;
 }
 
 PID::~PID() {}
 
-double PID::Update(double cte)
+double PID::Update(const double cte)
 {
-    std::time_t current_timestamp = time(nullptr);
-
-    // If this is the first observation, do not scale the derivative (AKA divide by 1).
-    // Otherwise, calculate the elapsed time in seconds.
-    double dt = ((current_timestamp != 0) ? (current_timestamp - prev_timestamp_) / 1e6 : 1.0);
-
     p_error_ = set_point_ - cte;
     d_error_ = (p_error_ - prev_error_);
     i_error_ += p_error_;
-
-    std::cout << "P:" << p_error_ << " I:" << i_error_ << " D:" << dt << std::endl;
 
     // prevent integral windup
     if (fabs(i_error_) > max_integral_)
         // multiply max_integral_ by the sign of the current integral
         i_error_ = max_integral_ * ((i_error_ > 0) ? 1.0 : ((i_error_ < 0) ? -1.0 : 0.0));
 
+//    std::cout << "P:" << p_error_ << " I:" << i_error_ << " D:" << d_error_ << std::endl;
+
     prev_error_ = p_error_;
-    prev_timestamp_ = current_timestamp;
 
     return Kp_ * p_error_ + Ki_ * i_error_ + Kd_ * d_error_;
+}
+
+inline
+void PID::resetTerms(const double kp_init, const double ki_init, const double kd_init)
+{
+    Kp_ = kp_init;
+    Ki_ = ki_init;
+    Kd_ = kd_init;
 }
--- PID-Controller/src/PID.h
+++ PID-Controller/src/PID.h
@@ -17,17 +17,22 @@ public:
     /**
      * Update the PID error variables given cross track error and return the estimated value.
      */
-    double Update(double cte);
+    double Update(const double cte);
 
     /**
      * Change the target point for the PID
      * */
-    void setTarget(double new_target) { set_point_ = new_target; }
+    void setTarget(const double new_target) { set_point_ = new_target; }
+
+    /**
+     * Change the terms of the PID
+     * */
+    void resetTerms(const double kp_init, const double ki_init, const double kd_init);
 
     /**
      * Set the maximum value for the integral term. Helps prevent integral windup.
      * */
-    void setMaxIntegral(double i_max) { max_integral_ = i_max; }
+    void setMaxIntegral(const double i_max) { max_integral_ = i_max; }
 
 private:
     // Target for the PID to converge to.
@@ -35,9 +40,6 @@ private:
 
     // error at t-1, used for derivative calculation
     double prev_error_;
-
-    // previous timestamp, used for dt calculation
-    std::time_t prev_timestamp_;
 
     // max integral term
     double max_integral_;
