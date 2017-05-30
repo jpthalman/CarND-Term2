f91935b80940034377c7b65a36ffffb96f2ed3c7
jpthalman <jpthalman@gmail.com>
2017-05-30 09:42:22 -0600

    Adjust formatting

--- ModelPredictiveControl/src/MPC.h
+++ ModelPredictiveControl/src/MPC.h
@@ -4,17 +4,19 @@
 #include <vector>
 #include "Eigen-3.3/Eigen/Core"
 
-using namespace std;
+using std::vector;
+using Eigen::VectorXd;
 
-class MPC {
- public:
-  MPC();
+class MPC
+{
+public:
+    MPC();
 
-  virtual ~MPC();
+    virtual ~MPC();
 
-  // Solve the model given an initial state and polynomial coefficients.
-  // Return the first actuatotions.
-  vector<double> Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs);
+    // Solve the model given an initial state and polynomial coefficients.
+    // Return the first actuatotions.
+    vector<double> Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs);
 };
 
 #endif /* MPC_H */
